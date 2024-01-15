54b8c2ce49fc4963229baa5df63d9e3c46d996a2
Austin Appleby <aappleby@gmail.com>
2024-01-14 21:28:14 -0800

    seems to be working with ProtoDict?

--- build.py
+++ build.py
@@ -1,162 +1,12 @@
 #!/usr/bin/python3
 
-import sys
-import glob
-import os
-import random
-import concurrent
-from concurrent.futures import ProcessPoolExecutor
-from concurrent.futures import ThreadPoolExecutor
-import asyncio
-import time
-from datetime import datetime
+import rules
+import re
+import pprint
 
-sys.path.append("symlinks/tinybuild")
-import tinybuild
-
-tinybuild.global_config["toolchain"]  = "x86_64-linux-gnu"
-tinybuild.global_config["build_type"] = "-g -O0"
-tinybuild.global_config["warnings"]   = "-Wunused-variable -Werror"
-tinybuild.global_config["depfile"]    = "-MMD -MF {file_out}.d"
-tinybuild.global_config["defines"]    = "-DCONFIG_DEBUG"
-tinybuild.global_config["cpp_std"]    = "-std=gnu++2a"
-
+from tinybuild import ProtoDict
 
 """
-_print=print
-def print(*args, **kw):
-    _print("[%s]" % (datetime.now()),*args, **kw)
-
-################################################################################
-
-proc_sem = asyncio.Semaphore(os.cpu_count())
-
-async def await_deps(input_deps):
-    dep_filenames = []
-    for f in input_deps:
-       if type(f) is asyncio.Task:
-        dep_filenames.append(await f)
-       else:
-        dep_filenames.append(f)
-    return dep_filenames
-
-#----------------------------------------
-
-async def dummy_compile(filename, time, input_deps = []):
-    dep_filenames = await await_deps(input_deps)
-
-    async with proc_sem:
-      print(f"start processing {filename} @ {time}")
-      print(dep_filenames)
-      proc = await asyncio.create_subprocess_shell(f"sleep {time}")
-      result = await proc.wait()
-      print(f"finished compiling {filename} @ {time} - result {result}")
-      return filename
-
-#----------------------------------------
-
-async def dummy_link(filename, input_deps = []):
-    print(f"    link {filename} waiting on futures")
-    dep_filenames = await await_deps(input_deps)
-    print(f"    link {filename} waiting on futures done")
-
-    #time = round(random.random(), 2)
-    time = 0
-    async with proc_sem:
-      print(f"    link {filename} starting")
-      print(dep_filenames)
-      proc = await asyncio.create_subprocess_shell(f"sleep {time}")
-      result = await proc.wait()
-      print(f"    finished linking {filename} @ {time} - result {result}")
-      return filename
-
-#----------------------------------------
-
-async def main():
-    futures1 = []
-    for i in range(20):
-      future = asyncio.create_task(dummy_compile(f"file{i}", round(random.random(), 2)))
-      futures1.append(future)
-
-    futures2 = []
-    for i in range(20):
-      future = asyncio.create_task(dummy_compile(f"slow file{i}", round(random.random() + 2, 2)))
-      futures2.append(future)
-
-    link1 = asyncio.create_task(dummy_link("bin1", futures1))
-    link2 = asyncio.create_task(dummy_link("bin2", futures2))
-
-    await asyncio.gather(link1, link2)
-
-    print(await link1)
-    print(await link2)
-
-#----------------------------------------
-
-if __name__ == "__main__":
-    asyncio.run(main())
-"""
-
-################################################################################
-
-compile_cpp = tinybuild.map_async(
-  desc      = "Compiling C++ {file_in} => {file_out}",
-  command   = "{toolchain}-g++ {opts} {includes} {defines} -c {file_in} -o {file_out}",
-  opts      = "{cpp_std} {warnings} {depfile} {build_type}",
-  includes  = "-Isymlinks/metrolib -Isymlinks/metron -I. -Isymlinks",
-)
-
-link_c_bin  = tinybuild.reduce_async(
-  desc      = "Linking {file_out}",
-  command   = "{toolchain}-g++ {opts} {join(files_in)} {join(deps)} {libraries} -o {file_out}",
-  opts      = "{build_type}",
-  deps      = [],
-  libraries = "",
-)
-
-async def build():
-  obj_main_o_futures = compile_cpp("src/main.cpp", "obj/main.o")
-  obj_test_o_futures = compile_cpp("src/test.cpp", "obj/test.o")
-
-  bin_main_futures = link_c_bin([obj_main_o_futures, obj_test_o_futures], "bin/main")
-
-  print(await obj_main_o_futures[0])
-  print(await obj_test_o_futures[0])
-  print(await bin_main_futures[0])
-
-asyncio.run(build())
-
-################################################################################
-
-"""
-
-compile_cpp = tinybuild.map(
-  desc      = "Compiling C++ {file_in} => {file_out}",
-  command   = "{toolchain}-g++ {opts} {includes} {defines} -c {file_in} -o {file_out}",
-  opts      = "{cpp_std} {warnings} {depfile} {build_type}",
-  includes  = "-Isymlinks/metrolib -Isymlinks/metron -I. -Isymlinks",
-)
-
-compile_c   = tinybuild.map(
-  desc      = "Compiling C {file_in} => {file_out}",
-  command   = "{toolchain}-gcc {opts} {includes} {defines} -c {file_in} -o {file_out}",
-  opts      = "{warnings} {depfile} {build_type}",
-  includes  = "-Isymlinks/metrolib -Isrc -I. -Isymlinks",
-)
-
-link_c_lib = tinybuild.reduce(
-  desc      = "Bundling {file_out}",
-  command   = "ar rcs {file_out} {join(files_in)}",
-)
-
-link_c_bin  = tinybuild.reduce(
-  desc      = "Linking {file_out}",
-  command   = "{toolchain}-g++ {opts} {join(files_in)} {join(deps)} {libraries} -o {file_out}",
-  opts      = "{build_type}",
-  deps      = [],
-  libraries = "",
-)
-
 def obj_name(x):
   return "obj/" + tinybuild.swap_ext(x, ".o")
 
@@ -165,10 +15,47 @@ def compile_dir(dir):
   objs  = [obj_name(x) for x in files]
   compile_cpp(files, objs)
   return objs
+"""
 
-obj_main_o_futures = compile_cpp("src/main.cpp", "obj/main.o")
-obj_test_o_futures = compile_cpp("src/test.cpp", "obj/test.o")
-link_c_bin([obj_main_o_futures, obj_test_o_futures], "bin/main")
+def build_main():
+  print("Building bin/main")
+  print("=====BEGIN=====")
+  rules.compile_cpp("src/test.cpp", "obj/test.o")
+  rules.compile_cpp("src/main.cpp", "obj/main.o")
+  rules.link_c_bin(["obj/main.o", "obj/test.o"], "bin/main")
+  print("=====END=====")
 
-tinybuild.finish()
+rules.tinybuild.build(build_main)
+
+"""
+config = ProtoDict()
+
+config.toolchain  = "x86_64-linux-gnu"
+config.build_type = "-g -O0"
+config.warnings   = "-Wunused-variable -Werror"
+config.depfile    = "-MMD -MF {file_out}.d"
+config.defines    = "-DCONFIG_DEBUG"
+config.cpp_std    = "-std=gnu++2a"
+config.includes   = "-I. -Isymlinks"
+config.c_opts     = "{warnings} {depfile} {build_type}"
+config.cpp_opts   = "{cpp_std} {c_opts}"
+config.ld_opts    = "{build_type}"
+
+rule = ProtoDict(config)
+rule.libraries = ""
+
+action = ProtoDict(rule)
+action.desc       = "Compiling C++ {file_in} => {file_out}"
+action.command    = "{toolchain}-g++ {cpp_opts} {includes} {defines} -c {file_in} -o {file_out}"
+
+file = ProtoDict(action)
+file.config = config
+file.rule = rule
+file.action = action
+file.files_in = ["src/main.cpp", "src/test.cpp"]
+file.files_out = ["obj/main.o", "obj/test.o"]
+
+print("========================================")
+file.dump()
+print("========================================")
 """
--- tinybuild.py
+++ tinybuild.py
@@ -2,46 +2,14 @@
 # 1 - Only rebuild files that need rebuilding
 # 2 - Make generating build commands simple
 
-# Build parameters can be specified globally, at rule scope, or at command
-# scope.
-
-################################################################################
+# Build parameters can be specified globally, at rule scope, or at action scope.
 
 import os
 import re
 import sys
-import multiprocessing
 import argparse
-import atexit
 import asyncio
-import inspect
-
 import pprint
-pp = pprint.PrettyPrinter(indent=2, width=120)
-
-# FIXME Emit ninja file?
-
-def swap_ext(name, new_ext):
-  return os.path.splitext(name)[0] + new_ext
-
-def join(names, divider = ' '):
-  return divider.join(names)
-
-def expand(text, arg_dict):
-  global global_config
-  if text is not None:
-    if global_config["debug"]: print(f"expand: {text}")
-    while re.search("{[^}]+}", text) is not None:
-      text = eval("f\"" + text + "\"", {}, arg_dict)
-      if global_config["debug"]: print(f"expand: {text}")
-  return text
-
-def listify(x):
-  if not type(x) is list: return [x]
-  result = []
-  for y in x:
-    result.extend(listify(y))
-  return result
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--verbose',  default=False, action='store_true', help='Print verbose build info')
@@ -49,366 +17,268 @@ parser.add_argument('--clean',    default=False, action='store_true', help='Dele
 parser.add_argument('--serial',   default=False, action='store_true', help='Do not parallelize commands')
 parser.add_argument('--dry_run',  default=False, action='store_true', help='Do not run commands')
 parser.add_argument('--debug',    default=False, action='store_true', help='Dump debugging information')
-options = parser.parse_args()
-
-global_config = {
-#  "verbose"    : options.verbose or options.debug,
-  "verbose"    : True,
-  "clean"      : options.clean,
-  "serial"     : options.serial,
-  "dry_run"    : options.dry_run,
-  #"debug"      : options.debug,
-  "debug"      : True,
-  "swap_ext"   : swap_ext,
-  "join"       : join
-}
+flags = parser.parse_args()
 
 ################################################################################
 
-def check_deps(files_in, file_out, file_kwargs):
-  file_out = expand(file_out, file_kwargs)
-  if not os.path.exists(file_out):
-    if file_kwargs['verbose']:
-      print(f"Rebuilding {file_out} because it's missing")
-    return True;
+class ProtoDict(object):
+  def __init__(self, obj=None):
+    self.proto = obj
 
-  for file_in in files_in:
-    file_in = expand(file_in, file_kwargs)
-    if os.path.exists(file_in):
+  def __getitem__(self, name):
+    if name in self.__dict__: return self.__dict__[name]
+    if self.proto: return self.proto[name]
+    return None
+
+  def __getattr__(self, name):
+    if name in self.__dict__: return self.__dict__[name]
+    if self.proto: return self.proto[name]
+    return None
+
+  #=============================================================================
+
+  def expand(self, text):
+    if self.verbose: print(f"expand: {text}")
+    if text is not None:
+      while re.search("{[^}]*}", text) is not None:
+        text = eval("f\"" + text + "\"", None, self)
+        if self.verbose: print(f"expand: {text}")
+    return text
+
+  #=============================================================================
+
+  def dump(self, depth=0):
+    for (k,v) in self.__dict__.items():
+      if k == "proto": continue
+      print(f"{'  ' * depth}{k} : ", end="")
+      if type(v) is ProtoDict:
+        print()
+        v.dump(depth + 1)
+      else:
+        print(v)
+
+  #=============================================================================
+
+  def needs_rebuild(self):
+
+    for file_in in self.files_in:
+      file_in = self.expand(file_in)
+      if not os.path.exists(file_in):
+        print(f"Input file {file_in} missing, aborting build!")
+        # Is there a better way to handle this?
+        sys.exit(-1)
+
+    for dep in self.deps:
+      dep = self.expand(dep)
+      if not os.path.exists(dep):
+        print(f"Dependency {dep} missing, aborting build!")
+        # Is there a better way to handle this?
+        sys.exit(-1)
+
+    if self.force: return True
+
+    for file_out in self.files_out:
+      file_out = self.expand(file_out)
+
+      # Check user-specified deps
+      if self.check_mtime(self.deps, file_out): return True
+
+      # Check depfile, if present
+      if os.path.exists(file_out + ".d"):
+        deplines = open(file_out + ".d").read().split()
+        deplines = [d for d in deplines[1:] if d != '\\']
+        if self.check_mtime(deplines, file_out):
+          return True
+
+      # Check input files
+      if self.check_mtime(self.files_in, file_out): return True
+
+      # All checks passed, don't need to rebuild this output
+      if self.verbose: print(f"File {self.file_out} is up to date")
+
+    return False
+
+  #=============================================================================
+
+  def check_mtime(self, files_in, file_out):
+
+    file_out = self.expand(file_out)
+    if not os.path.exists(file_out):
+      if self.verbose: print(f"Rebuilding {file_out} because it's missing")
+      return True
+
+    for file_in in files_in:
+      file_in = self.expand(file_in)
       if os.path.getmtime(file_in) > os.path.getmtime(file_out):
-        if file_kwargs['verbose']:
-          print(f"Rebuilding {file_out} because it's older than dependency {file_in}")
-        return True
-    else:
-      print(f"Dependency {file_in} missing, aborting build!")
-      # Is there a better way to handle this?
-      sys.exit(-1)
-  return False
-
-################################################################################
-
-def needs_rebuild(files_in, files_out, file_kwargs):
-  if file_kwargs.get("force", False):
-    return True
-
-  files_in  = listify(files_in)
-  files_out = listify(files_out)
-
-  for file_out in files_out:
-    file_out = expand(file_out, file_kwargs)
-
-    # Check user-specified deps
-    if check_deps(file_kwargs.get("deps", []), file_out, file_kwargs):
-      return True
-
-    # Check depfile, if present
-    if os.path.exists(file_out + ".d"):
-      deps = open(file_out + ".d").read().split()
-      deps = [d for d in deps[1:] if d != '\\']
-      if check_deps(deps, file_out, file_kwargs):
+        if self.verbose: print(f"Rebuilding {file_out} because it's older than dependency {file_in}")
         return True
 
-    # Check input files
-    if check_deps(files_in, file_out, file_kwargs):
-      return True
+    return False
 
-    # All checks passed, don't need to rebuild this output
-    if file_kwargs.get("verbose", False):
-      print(f"File {file_out} is up to date")
 
-  return False
 
 ################################################################################
 
-proc_sem = asyncio.Semaphore(os.cpu_count())
+def swap_ext(name, new_ext):
+  return os.path.splitext(name)[0] + new_ext
 
-#----------------------------------------
+def join(names, divider = ' '):
+  return divider.join(names)
 
-async def await_deps(input_deps):
-  input_deps = listify(input_deps)
+# Repeatedly expands 'text' as if it was a f-string until it contains no {}s.
+"""
+def expand(text, arg_dict, verbose = False):
+  if text is not None:
+    if verbose: print(f"expand: {text}")
+    while re.search("{[^}]*}", text) is not None:
+      text = eval("f\"" + text + "\"", {}, arg_dict)
+      if verbose: print(f"expand: {text}")
+  return text
+"""
 
-  dep_filenames = []
-  for f in input_deps:
-    if inspect.isawaitable(f): f = await f
-    dep_filenames.append(f)
-  return listify(dep_filenames)
-
-#----------------------------------------
-
-async def run_command_async(file_kwargs):
-  files_in  = await await_deps(file_kwargs["files_in"])
-  files_out = file_kwargs["files_out"]
-
-  file_in = file_kwargs["file_in"]
-  if inspect.isawaitable(file_in):
-    file_in = await file_in
-    file_kwargs["file_in"] = file_in
-
-  file_kwargs["files_in"] = files_in
-
-  for f in files_in:
-    if not type(f) is str:
-      print(f"run_command_async got non-string {f}")
-      sys.exit(-1)
-
-  if not needs_rebuild(files_in, files_out, file_kwargs):
-    return files_out
-
-  if desc := file_kwargs.get("desc", None):
-    print(expand(desc, file_kwargs))
-
-  if file_kwargs["debug"]:
-    pp.pprint(file_kwargs)
-
-  command = expand(file_kwargs["command"], file_kwargs)
-  result = -1
-
-  if file_kwargs["verbose"]:
-    print(f"Command starting: \"{command}\"")
-
-  if file_kwargs.get("dry_run", False):
-    print(f"Dry run: \"{command}\"")
-    return files_out
-
-  async with proc_sem:
-    proc = await asyncio.create_subprocess_shell(command)
-    result = await proc.wait()
-
-  if result:
-    print(f"Command failed: \"{command}\"")
-  elif file_kwargs["verbose"]:
-    print(f"Command done: \"{command}\"")
-
-  return files_out
-
-################################################################################
-
-def run_command(file_kwargs):
-
-  files_in  = file_kwargs["files_in"]
-  files_out = file_kwargs["files_out"]
-
-  if not needs_rebuild(files_in, files_out, file_kwargs):
-    return 0
-
-  if desc := file_kwargs.get("desc", None):
-    print(expand(desc, file_kwargs))
-
-  if file_kwargs["debug"]:
-    pp.pprint(file_kwargs)
-
-  command = expand(file_kwargs["command"], file_kwargs)
-  result = -1
-
-  if file_kwargs["verbose"]:
-    print(f"Command starting: \"{command}\"")
-
-  if file_kwargs.get("dry_run", False):
-    print(f"Dry run: \"{command}\"")
-    return 0
-
-  result = os.system(command)
-  if result:
-    print(f"Command failed: \"{command}\"")
-  elif file_kwargs["verbose"]:
-    print(f"Command done: \"{command}\"")
-
+# Wraps scalars in a list, flattens nested lists into a single list.
+def listify(x):
+  if x is None: return []
+  if not type(x) is list: return [x]
+  result = []
+  for y in x: result.extend(listify(y))
   return result
 
 ################################################################################
 
-def create_rule_async(do_map, do_reduce, kwargs):
-  # Take a snapshot of the global config at the time the rule is defined
-  global_kwargs = dict(global_config)
-
-  # Take a snapshot of the config kwargs and patch in our rule kwargs
-  rule_kwargs = dict(global_kwargs)
-  rule_kwargs.update(kwargs)
-  #rule_kwargs["global_args"] = global_kwargs;
-
-  #----------
-
-  def rule(files_in, files_out, **kwargs):
-    files_in  = listify(files_in)
-    files_out = listify(files_out)
-
-    # Make sure our output directories exist
-    for file_out in files_out:
-      if dirname := os.path.dirname(file_out):
-        os.makedirs(dirname, exist_ok = True)
-
-    if do_map:
-      assert len(files_in) == len(files_out)
-
-    # Take a snapshot of the rule kwargs and patch in our command kwargs
-    command_kwargs = dict(rule_kwargs)
-    command_kwargs.update(kwargs)
-    #command_kwargs["rule_args"] = rule_kwargs
-    command_kwargs["files_in"]  = files_in
-    command_kwargs["files_out"] = files_out
-
-    ########################################
-    # Clean files if requested
-
-    if command_kwargs.get("clean", None):
-      for file_out in command_kwargs["files_out"]:
-        file_out = expand(file_out, command_kwargs)
-        if command_kwargs.get("verbose", False):
-          print(f"rm -f {file_out}")
-        os.system(f"rm -f {file_out}")
-      return []
-
-    ########################################
-    # Dispatch the command as a map
-
-    if do_map:
-      results = []
-      for i in range(len(files_in)):
-        file_in  = files_in[i]
-        file_out = files_out[i]
-
-        file_kwargs = dict(command_kwargs)
-        file_kwargs["file_in"]  = file_in
-        file_kwargs["file_out"] = file_out
-        #file_kwargs["rule_args"] = rule_kwargs
-        #file_kwargs["global_args"] = global_kwargs
-
-        if file_kwargs["serial"]:
-          run_command(file_kwargs)
-        else:
-          result = asyncio.create_task(run_command_async(file_kwargs))
-          results.append(result)
-      return results
-
-    ########################################
-    # Dispatch the command as a reduce
-
-    if do_reduce:
-      file_kwargs = dict(command_kwargs)
-      file_kwargs["file_in"]  = files_in[0]
-      file_kwargs["file_out"] = files_out[0]
-      result = asyncio.create_task(run_command_async(file_kwargs))
-      return [result]
-
-  #----------
-
-  return rule
+global_args = ProtoDict()
+global_args.verbose  = flags.verbose or flags.debug
+global_args.clean    = flags.clean
+global_args.serial   = flags.serial
+global_args.dry_run  = flags.dry_run
+global_args.debug    = flags.debug
+global_args.swap_ext = swap_ext
+global_args.join     = join
 
 ################################################################################
 
-def create_rule(do_map, do_reduce, kwargs):
-  # Take a snapshot of the global config at the time the rule is defined
-  global_kwargs = dict(global_config)
+proc_sem = asyncio.Semaphore(1 if flags.serial else os.cpu_count())
 
-  # Take a snapshot of the config kwargs and patch in our rule kwargs
-  rule_kwargs = dict(global_kwargs)
-  rule_kwargs.update(kwargs)
-  rule_kwargs["global_args"] = global_kwargs
+promise_map = {}
 
-  def rule(files_in, files_out, **kwargs):
-    files_in  = listify(files_in)
-    files_out = listify(files_out)
+################################################################################
+
+async def run_command_async(args):
+
+  for f in args.files_in:
+    if promise := promise_map.get(f, None):
+      dep_result = await promise
+      if dep_result != 0: return dep_result
+
+  for d in args.deps:
+    if promise := promise_map.get(d, None):
+      dep_result = await promise
+      if dep_result != 0: return dep_result
+
+  if not args.needs_rebuild(): return 0
+
+  if args.desc: print(args.expand(args.desc))
+
+  if args.debug: args.dump(1)
+
+  command = args.expand(args.command)
+
+  if args.dry_run:
+    print(f"Dry run: \"{command}\"")
+    return 0
+
+  if args.verbose: print(f"Command starting: \"{command}\"")
+
+  async with proc_sem:
+    proc = await asyncio.create_subprocess_shell(
+      command,
+      stdout = asyncio.subprocess.PIPE,
+      stderr = asyncio.subprocess.PIPE)
+    (stdout_data, stderr_data) = await proc.communicate()
+
+  if proc.returncode != 0:
+    print(f"Command failed: \"{command}\"")
+    stderr_text = stderr_data.decode()
+    if len(stderr_text): print(f"stderr =\n{stderr_text}")
+  elif args.verbose:
+    print(f"Command done: \"{command}\"")
+    stdout_text = stdout_data.decode()
+    if len(stdout_text): print(f"stdout =\n{stdout_text}")
+
+  return proc.returncode
+
+################################################################################
+
+def create_rule(do_map, do_reduce, rule_dict):
+
+  rule_args = ProtoDict(global_args)
+  rule_args.__dict__.update(rule_dict)
+
+  def run_rule(_files_in, _files_out, **kwargs):
+
+    # Take a snapshot of the rule args and patch in our command args
+    action_args = ProtoDict(rule_args)
+    action_args.__dict__.update(kwargs)
+    action_args.files_in  = listify(_files_in)
+    action_args.files_out = listify(_files_out)
+    action_args.deps      = listify(action_args.deps)
+    action_args.rule      = rule_args
+
+    # Clean files if requested
+    if action_args.clean:
+      for file_out in action_args.files_out:
+        file_out = action_args.expand(file_out)
+        if action_args.verbose:
+          print(f"rm -f {file_out}")
+        os.system(f"rm -f {file_out}")
+      return
 
     # Make sure our output directories exist
-    for file_out in files_out:
+    for file_out in action_args.files_out:
       if dirname := os.path.dirname(file_out):
         os.makedirs(dirname, exist_ok = True)
 
-    if do_map:
-      assert len(files_in) == len(files_out)
-
-    # Take a snapshot of the rule kwargs and patch in our command kwargs
-    command_kwargs = dict(rule_kwargs)
-    command_kwargs.update(kwargs)
-    command_kwargs["rule_args"] = rule_kwargs
-    command_kwargs["files_in"]  = files_in
-    command_kwargs["files_out"] = files_out
-
-    ########################################
-    # Clean files if requested
-
-    if command_kwargs.get("clean", None):
-      for file_out in command_kwargs["files_out"]:
-        file_out = expand(file_out, command_kwargs)
-        if command_kwargs.get("verbose", False):
-          print(f"rm -f {file_out}")
-        os.system(f"rm -f {file_out}")
-      return []
-
-    results = []
-
-    ########################################
     # Dispatch the command as a map
-
     if do_map:
-      for i in range(len(files_in)):
-        file_in  = files_in[i]
-        file_out = files_out[i]
+      assert len(action_args.files_in) == len(action_args.files_out)
+      for i in range(len(action_args.files_in)):
+        command_args = ProtoDict(action_args)
+        command_args.file_in  = action_args.files_in[i]
+        command_args.file_out = action_args.files_out[i]
+        command_args.action   = action_args
 
-        file_kwargs = dict(command_kwargs)
-        file_kwargs["file_in"]      = file_in
-        file_kwargs["file_out"]     = file_out
+        promise = asyncio.create_task(run_command_async(command_args))
+        promise_map[file_out] = promise
+      return
 
-        if file_kwargs["serial"]:
-          run_command(file_kwargs)
-        else:
-          result = pool.apply_async(run_command, [file_kwargs])
-          results.append(result)
-
-    ########################################
-    # Dispatch the command as a reduce
-
+    # Or dispatch the command as a reduce
     if do_reduce:
-      file_kwargs = dict(command_kwargs)
-      file_kwargs["file_in"]  = files_in[0]
-      file_kwargs["file_out"] = files_out[0]
-      result = pool.apply_async(run_command, [file_kwargs])
-      results.append(result)
+      command_args = ProtoDict(action_args)
+      command_args.file_in  = action_args.files_in[0]
+      command_args.file_out = action_args.files_out[0]
+      command_args.action   = action_args
+      promise = asyncio.create_task(run_command_async(command_args))
+      for file_out in action_args.files_out:
+        promise_map[file_out] = promise
+      return
 
-    ########################################
-    # Block until all tasks done
+  #----------
 
-    sum = 0
-    for result in results:
-      r = result.get()
-      sum = sum + result.get()
-    if sum:
-      print("Command failed, aborting build")
-      sys.exit(-1)
-
-    return files_out
-
-  ########################################
-
-  return rule
+  return run_rule
 
 ################################################################################
 
 def map(**kwargs):
-  return create_rule(do_map = True, do_reduce = False, kwargs = kwargs)
+  return create_rule(do_map = True, do_reduce = False, rule_dict = kwargs)
 
 def reduce(**kwargs):
-  return create_rule(do_map = False, do_reduce = True, kwargs = kwargs)
+  return create_rule(do_map = False, do_reduce = True, rule_dict = kwargs)
 
-def map_async(**kwargs):
-  return create_rule_async(do_map = True, do_reduce = False, kwargs = kwargs)
+async def top(build_func):
+  build_func()
+  await asyncio.gather(*asyncio.all_tasks() - {asyncio.current_task()})
 
-def reduce_async(**kwargs):
-  return create_rule_async(do_map = False, do_reduce = True, kwargs = kwargs)
-
-def finish():
-  pool.close()
-  pool.join()
-
-"""
-def blah():
-  pool.close()
-  pool.join()
-
-atexit.register(blah)
-"""
+def build(build_func):
+  asyncio.run(top(build_func))
 
 ################################################################################
-
-# Warning - this _must_ be done _after_ the rest of the module is initialized
-pool = multiprocessing.Pool(multiprocessing.cpu_count())
