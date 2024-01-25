ebe66160f0bdff743e24c3637a6367af0d37a03d
Austin Appleby <aappleby@gmail.com>
2024-01-24 19:48:47 -0800

    checkpoint

--- build.py
+++ build.py
@@ -14,67 +14,49 @@ parser = argparse.ArgumentParser(
 )
 
 parser.add_argument('--verbose',  default=False, action='store_true', help='Print verbose build info')
-parser.add_argument('--clean',    default=False, action='store_true', help='Delete intermediate files')
 parser.add_argument('--serial',   default=False, action='store_true', help='Do not parallelize commands')
 parser.add_argument('--dry_run',  default=False, action='store_true', help='Do not run commands')
 parser.add_argument('--debug',    default=False, action='store_true', help='Dump debugging information')
 parser.add_argument('--dotty',    default=False, action='store_true', help='Dump dependency graph as dotty')
 (flags, unrecognized) = parser.parse_known_args()
 
-hancho.config.verbose    = flags.verbose
-hancho.config.clean      = flags.clean
-hancho.config.serial     = flags.serial
-hancho.config.dry_run    = flags.dry_run
-hancho.config.debug      = flags.debug
-hancho.config.dotty      = flags.dotty
-
-#hancho.config.toolchain  = "x86_64-linux-gnu"
-#hancho.config.build_opt  = "-g -O0"
-#hancho.config.warnings   = "-Wunused-variable -Werror"
-#hancho.config.depfile    = "-MMD -MF {file_out}.d"
-#hancho.config.defines    = "-DCONFIG_DEBUG"
-#hancho.config.cpp_std    = "-std=gnu++2a"
-#hancho.config.includes   = "-I. -Isymlinks"
-#hancho.config.c_opts     = "{warnings} {depfile} {build_opt}"
-#hancho.config.cpp_opts   = "{cpp_std} {c_opts}"
-#hancho.config.ld_opts    = "{build_opt}"
-
-compile_cpp = hancho.rule(
-  #desccription = "Compiling C++ {file_in} => {file_out}",
-  command  = "{toolchain}-g++ {cpp_opts} {includes} {defines} -c {file_in} -o {file_out}",
-  parallel = True
+base_config = hancho.Config(
+  prototype = hancho.config,
+  verbose   = flags.verbose,
+  serial    = flags.serial,
+  dry_run   = flags.dry_run,
+  debug     = flags.debug,
+  dotty     = flags.dotty,
+  toolchain = "x86_64-linux-gnu",
+  build_opt = "-g -O0",
+  warnings  = "-Wunused-variable -Werror",
+  depfile   = "-MMD -MF {file_out}.d",
+  defines   = "-DCONFIG_DEBUG",
+  includes  = "-I. -Isymlinks",
 )
 
-compile_c = hancho.rule(
-  #desccription = "Compiling C {file_in} => {file_out}",
-  command  = "{toolchain}-gcc {c_opts} {includes} {defines} -c {file_in} -o {file_out}",
-  parallel = True
+rule_compile_cpp = hancho.Config(
+  prototype = base_config,
+  command   = "{toolchain}-g++ {cpp_std} {warnings} {depfile} {build_opt} {includes} {defines} -c {file_in} -o {file_out}",
+  cpp_std   = "-std=gnu++2a",
 )
 
-link_c_lib = hancho.rule(
-  #desccription = "Bundling {file_out}",
-  command = "ar rcs {file_out} {join(files_in)}",
+compile_c = hancho.Config(
+  prototype = base_config,
+  command   = "{toolchain}-gcc {warnings} {depfile} {build_opt} {includes} {defines} -c {file_in} -o {file_out}",
 )
 
-link_c_bin = hancho.rule(
-  #desccription = "Linking {file_out}",
-  command = "{toolchain}-g++ {ld_opts} {join(files_in)} {libraries} -o {file_out}",
+link_c_lib = hancho.Config(
+  prototype = base_config,
+  command   = "ar rcs {file_out} {join(files_in)}",
 )
 
-compile_cpp(files_in = "src/test.cpp", files_out = "obj/test.o")
-compile_cpp(files_in = "src/main.cpp", files_out = "obj/main.o")
+link_c_bin = hancho.Config(
+  prototype = base_config,
+  command   = "{toolchain}-g++ {build_opt} {join(files_in)} {libs} -o {file_out}",
+  libs      = "",
+)
+
+rule_compile_cpp(files_in = "src/test.cpp", files_out = "obj/test.o")
+rule_compile_cpp(files_in = "src/main.cpp", files_out = "obj/main.o")
 link_c_bin(files_in = ["obj/main.o", "obj/test.o"], files_out = "bin/main")
-
-
-
-
-"""
-def obj_name(x):
-  return "obj/" + hancho.swap_ext(x, ".o")
-
-def compile_dir(dir):
-  files = glob.glob(dir + "/*.cpp") + glob.glob(dir + "/*.c")
-  objs  = [obj_name(x) for x in files]
-  compile_cpp(files, objs)
-  return objs
-"""
--- hancho.py
+++ hancho.py
@@ -23,52 +23,36 @@ Build parameters can be specified globally, at rule scope, or at action scope.
 >>> hancho.run(my_task)
 """
 
-import asyncio
-import atexit
-import os
-import re
-import sys
-import subprocess
-from functools import partial
+import asyncio, os, re, sys, subprocess
 
 hancho_loop  = asyncio.new_event_loop()
-hancho_tasks = []
+hancho_queue = []
+hancho_abort = False
 
 ################################################################################
 # Minimal JSON-style pretty printer for Config
 
-def dump_dict(d, depth):
-  print("{")
+def repr_dict(d, depth):
+  result = "{\n"
   for (k,v) in d.items():
-    #if k == "prototype": continue
-    print("  " * (depth + 1), end="")
-    print(f"\"{k}\" : ", end="")
-    dump_val(v, depth + 1)
-    print(",")
-  print(("  " * depth) + "}", end="")
+    result += "  " * (depth + 1)
+    result += repr_val(k, depth + 1)
+    result += " : "
+    result += repr_val(v, depth + 1)
+    result += ",\n"
+  result += "  " * depth + "}"
+  return result
 
-def dump_list(l, depth):
-  print("[", end="")
-  for s in l:
-    dump_val(s, depth)
-    print(", ", end="");
-  print("]", end="")
+def repr_list(l, depth):
+  return str([repr_val(v, depth) for v in l])
 
-def dump_val(v, depth):
-  if   v is None:            print("null", end="")
-  elif type(v) is Config:
-    try:
-      self_name = object.__getattribute__(v, 'name')
-    except AttributeError:
-      self_name = "[?name?]"
-    print(f"{self_name} ", end="")
-    dump_dict(object.__getattribute__(v, "__dict__"), depth)
-
-  elif type(v) is str:       print(f"\"{v}\"", end="")
-  elif type(v) is dict:      dump_dict(v, depth + 1)
-  elif type(v) is list:      dump_list(v, depth + 1)
-  elif callable(v):          print(f"\"{v}\"", end="")
-  else:                      print(v, end="")
+def repr_val(v, depth):
+  if v is None:           return "null"
+  elif type(v) is str:    return '"' + v + '"'
+  elif type(v) is dict:   return repr_dict(v, depth + 1)
+  elif type(v) is list:   return repr_list(v, depth + 1)
+  elif type(v) is Config: return repr_dict(v.__dict__, depth)
+  else:                   return str(v)
 
 ################################################################################
 
@@ -114,76 +98,32 @@ def listify(x):
 
 template_regex = re.compile("{[^}]*}")
 
-def is_template(text):
-  return type(text) is str and template_regex.search(text) is not None
-
-#-------------------------------------------------------------------------------
-
-expand_depth = 0
-
-def expand(text, context):
-  global expand_depth
-  debug = resolve(context, "debug")
-  if text is None:
-    return ""
-
+def expand(text, context, debug = False):
   if type(text) is list:
-    return [expand(t, context) for t in text]
+    return [expand(t, context, debug) for t in text]
 
-  if is_template(text):
-    while True:
-      if debug:
-        try:
-          self_name = object.__getattribute__(context, "name")
-        except AttributeError:
-          self_name = "[?name?]"
-        print(f"{'  ' * expand_depth}{self_name}.expand \"{text}\"")
-      expand_depth = expand_depth + 1
-      try:
-        text = eval("f\"" + text + "\"", globals(), context)
-      except AttributeError as e:
-        break
-      expand_depth = expand_depth - 1
-      if not is_template(text): break
-    if debug: print(f"{'  ' * expand_depth }result \"{text}\"")
-    return text
-
-  return text
-
-#-------------------------------------------------------------------------------
-
-def resolve(self, name):
-  #print(f"resolve {name}")
-  result = None
-  cursor = self
-  while cursor is not None:
-    # See if the current object in the prototype chain contains {name}
+  while template_regex.search(text):
+    if debug: print(f"expand \"{text}\"")
     try:
-      result = object.__getattribute__(cursor, name)
-      if result is not None: break
-    except AttributeError:
-      pass
-
-    # Didn't find it, step up the prototype chain.
-    try:
-      cursor = object.__getattribute__(cursor, "prototype")
-    except AttributeError:
+      text = eval("f\"" + text + "\"", None, context)
+    except:
       break
 
-  # Context lookup failed, check globals.
-  if result is None:
-    if name in globals():
-      return globals()[name]
-    if name in globals()["__builtins__"]:
-      return globals()["__builtins__"][name]
-
-  if result is None:
-    raise AttributeError(f"Could not resolve attribute {name}")
-  return result
+  if debug: print(f"result \"{text}\"")
+  return text
 
 ################################################################################
 
-config = None
+def resolve(self, name):
+  try:
+    return object.__getattribute__(self, name)
+  except AttributeError:
+    if proto := object.__getattribute__(self, "prototype"):
+      return resolve(proto, name)
+    else:
+      raise AttributeError(f"Could not resolve attribute {name} for {self}")
+
+################################################################################
 
 class Config(object):
   """
@@ -218,51 +158,19 @@ class Config(object):
   """
 
   #----------------------------------------
+
   def __init__(self, *, prototype, **kwargs):
-    for name in kwargs:
-        setattr(self, name, kwargs[name])
+    for name in kwargs: setattr(self, name, kwargs[name])
     self.prototype = prototype
 
-  #----------------------------------------
-
   def __getitem__(self, name):
-    result = resolve(self, name)
-    result = expand(result, self)
-    #self_name = object.__getattribute__(self, "name")
-    #print(f"__getitem__ {self_name} {name} = \"{result}\"")
-    return result
-
-  #----------------------------------------
+    return resolve(self, name)
 
   def __getattribute__(self, name):
-    result = resolve(self, name)
-    result = expand(result, self)
-    #self_name = object.__getattribute__(self, "name")
-    #print(f"__getattribute__ {self_name} {name} = \"{result}\"")
-    return result
+    return resolve(self, name)
 
-  #----------------------------------------
-
-  def dump(self, depth = 0):
-    dump_val(self, depth)
-    print()
-
-  #----------------------------------------
-
-  def queue(self):
-    if all(output in promise_map for output in self.files_out):
-      #print(f"####### Output already built for {self.files_out}")
-      return
-
-    if any(output in promise_map for output in self.files_out):
-      print(f"####### Overlapping output sets for {self.files_out}")
-      sys.exit(-1)
-
-    coroutine = self.run_command_async()
-    promise = hancho_loop.create_task(coroutine)
-    hancho_tasks.append(promise)
-    for output in self.files_out:
-      promise_map[output] = promise
+  def __repr__(self):
+    return repr_val(self, 0)
 
   #----------------------------------------
 
@@ -275,9 +183,6 @@ class Config(object):
   #----------------------------------------
 
   def needs_rebuild(self):
-
-    #if not self.files_out:
-    #  return f"Rebuilding {self.file_in} because it has no outputs"
 
     for file_out in self.files_out:
       # Check for missing outputs.
@@ -300,7 +205,8 @@ class Config(object):
         return f"Rebuilding {file_out} because an input has changed"
 
       # All checks passed, so we don't need to rebuild this output.
-      if self.debug: print(f"File {self.file_out} is up to date")
+      if self.debug:
+        print(f"File {self.files_out} is up to date")
 
     # All deps were up-to-date, nothing to do.
     sys.stdout.flush()
@@ -308,76 +214,25 @@ class Config(object):
 
   #----------------------------------------
 
-  async def check_deps(self, deps):
+  async def wait_for_deps(self, deps):
+    global promise_map
     for file in deps:
       if promise := promise_map.get(file, None):
         dep_result = await promise
         if dep_result != 0: return dep_result
       if file and not os.path.exists(file):
-        if self.description:
-          print(self.description)
-        else:
-          print(self.command)
         print(f"Dependency {file} missing!")
         sys.stdout.flush()
         return -1
     pass
 
   #----------------------------------------
-  # Our generic rule dispatcher has dispatch_as_map/rule_args bound by
-  # create_rule(), and the rest are provided at rule invocation.
-
-  def __call__(self, *, files_in, files_out, deps, **kwargs):
-
-    assert type(files_in) is list
-    assert type(files_out) is list
-    assert type(deps) is list
-
-    files_in  = listify(files_in)
-    files_out = listify(files_out)
-    deps      = listify(deps)
-
-    call_args = Config(
-      prototype = self,
-      name      = "call_args",
-      #file_in   = files_in[0],
-      #file_out  = files_out[0] if len(files_out) else "",
-      files_in  = files_in,
-      files_out = files_out,
-      deps      = deps,
-      **kwargs,
-    )
-
-    # Print dotty graph if requested
-    if call_args.dotty:
-      for file_in in call_args.files_in:
-        for file_out in call_args.files_out:
-          print(f"  \"{file_in}\" -> \"{file_out}\";")
-      return
-
-    # Clean files if requested
-    if call_args.clean:
-      for file_out in call_args.files_out:
-        if call_args.verbose:
-          print(f"rm -f {file_out}")
-        os.system(f"rm -f {file_out}")
-      return
-
-    # Make sure our output directories exist
-    for file_out in call_args.files_out:
-      if dirname := os.path.dirname(file_out):
-        os.makedirs(dirname, exist_ok = True)
-
-    # OK, we can queue up the rule now.
-    call_args.queue()
-
-  #----------------------------------------
 
   async def run_command_async(self):
 
     # Wait on all our dependencies to be updated
-    await self.check_deps(self.files_in)
-    await self.check_deps(self.deps)
+    await self.wait_for_deps(self.files_in)
+    await self.wait_for_deps(self.deps)
 
     # Our dependencies are ready, we can grab a process semaphore slot now.
     async with proc_sem:
@@ -401,39 +256,46 @@ class Config(object):
 
       if not reason: return 0
 
+      # Add a gap after the last task if verbose
+      if self.verbose or self.debug:
+        print()
+
+      # Print description
+      description = expand(self.description, self)
+      if self.verbose or self.debug:
+        print(f"[{node_visit}/{node_total}] {description}")
+      else:
+        print("\r", end="")
+        status = f"[{node_visit}/{node_total}] {description}"
+        status = status[:os.get_terminal_size().columns - 1]
+        print(status, end="")
+        print("\x1B[K", end="")
+
+      # Print rebuild reason
       if self.debug: print(reason)
       sys.stdout.flush()
 
-      #----------
       # Print debug dump of args if needed
-      if self.debug:
-        self.dump()
+      if self.debug: print(self)
 
-      # Expand "command" as late as possible, just in case some previous action
-      # changed some arg somehow.
-      command = expand(self.command, self)
+      # Print command
+      command = expand(self.command, self, self.debug)
       if not command:
         print(f"Command missing for input {self.file_in}!")
         sys.stdout.flush()
         return -1
-
-      # Print description
-
       if self.verbose or self.debug:
-        print(f"[{node_visit}/{node_total}] {self.description}")
         print(f"{command}")
-        print()
-      else:
-        print("\r", end="")
-        status = f"[{node_visit}/{node_total}] {self.description}"
-        status = status[:os.get_terminal_size().columns - 1]
-        print(status, end="")
-        print("\x1B[K", end="")
 
       sys.stdout.flush()
 
       # Early-exit if this is just a dry run
       if self.dry_run: return 0
+
+      # Make sure our output directories exist
+      for file_out in self.files_out:
+        if dirname := os.path.dirname(file_out):
+          os.makedirs(dirname, exist_ok = True)
 
       # OK, we're ready to start the subprocess. In serial mode we run the
       # subprocess synchronously.
@@ -462,7 +324,7 @@ class Config(object):
 
       # If the command failed, print stdout/stderr and return the error code.
       if returncode != 0:
-        if not self.verbose: print()
+        if not (self.verbose or self.debug): print()
         print(f"\x1B[31mFAILED\x1B[0m: {self.files_out}")
         print(command)
         print(stderr, end="")
@@ -470,16 +332,56 @@ class Config(object):
         sys.stdout.flush()
         return returncode
 
-      if stdout or stderr:
-        if not self.verbose: print()
-        print(stderr, end="")
-        print(stdout, end="")
+      if self.verbose or not self.quiet:
+        if stdout or stderr:
+          if not self.verbose: print()
+          print(stderr, end="")
+          print(stdout, end="")
 
       node_built = node_built + 1
 
       sys.stdout.flush()
 
       return returncode
+
+  #----------------------------------------
+
+  def __call__(self, **kwargs):
+
+    call_args = Config(
+      prototype = self,
+      name = "call_args",
+      **kwargs)
+
+    queue(call_args)
+
+################################################################################
+
+def queue(command):
+  global promise_map
+
+  # Expand all filenames
+  command.files_in  = [expand(file, command, command.debug) for file in listify(command.files_in)]
+  command.files_out = [expand(file, command, command.debug) for file in listify(command.files_out)]
+  command.deps      = [expand(file, command, command.debug) for file in listify(command.deps)]
+
+  # Check for duplicate outputs
+  for file_out in command.files_out:
+    if file_out in promise_map:
+      global hancho_abort
+      print(f"####### Multiple rules build {file_out}!")
+      hancho_abort = True
+      sys.exit()
+
+  # Print dotty graph if requested
+  if command.dotty:
+    for file_in in command.files_in:
+      for file_out in command.files_out:
+        print(f"  \"{file_in}\" -> \"{file_out}\";")
+    return
+
+  # OK, we can queue up the rule now.
+  hancho_queue.append(command)
 
 
 ################################################################################
@@ -490,24 +392,30 @@ Special action args
   command:     Command to run for the rule
   files_in:    Either a single filename or a list of filenames
   files_out:   Either a single filename or a list of filenames
-  deps:        Additional dependencies for the rule
   force:       Makes the rule always run even if dependencies are up to date
 """
 
 config = Config(
   prototype = None,
+  name      = "hancho.config",
   verbose   = False, # Print verbose build info
-  clean     = False, # Delete intermediate files
+  quiet     = False, # Don't print command results
   serial    = False, # Do not parallelize commands
   dry_run   = False, # Do not run commands
-  debug     = False, # Dump debugging information
-  dotty     = False, # Dump dependency graph as dotty
+  debug     = False, # Print debugging information
+  dotty     = False, # Print dependency graph as dotty instead of building
 
-  description = "{command}",
-  command   = "echo You forgot the command for {file_out}",
-  file_in   = "{files_in[0]}",
-  file_out  = "{files_out[0] if len(files_out) else ''}",
-  force     = False,
+  description = "{files_in} -> {files_out}",
+  command     = "echo You forgot the command for {file_out}",
+  file_in     = "{files_in[0]}",
+  file_out    = "{files_out[0] if len(files_out) else ''}",
+  deps        = [],
+  force       = False,
+
+  join     = join,
+  len      = len,
+  swap_ext = swap_ext,
+  listify  = listify
 )
 
 node_total = 0
@@ -519,37 +427,60 @@ proc_sem = None
 
 ################################################################################
 
-any_failed = False
-
-def hancho_atexit():
-  global node_visit
-  global node_total
+def reset():
+  global hancho_abort
+  global hancho_queue
   global node_built
+  global node_total
+  global node_visit
   global proc_sem
+  global promise_map
 
-  all_tasks = asyncio.all_tasks(hancho_loop)
+  hancho_abort = False
+  hancho_queue.clear()
+  node_built = 0
+  node_total = 0
+  node_visit = 0
+  proc_sem = None
+  promise_map.clear()
 
-  if not all_tasks:
-    return
+################################################################################
+
+def build():
+  global hancho_abort
+  global hancho_queue
+  global node_built
+  global node_total
+  global proc_sem
+  global promise_map
+
+  if hancho_abort or not hancho_queue:
+    reset()
+    return False
+
+  hancho_tasks = []
+  for command in hancho_queue:
+    coroutine = command.run_command_async()
+    promise = hancho_loop.create_task(coroutine)
+    hancho_tasks.append(promise)
+    for output in command.files_out:
+      promise_map[output] = promise
 
   if proc_sem is None:
     proc_sem = asyncio.Semaphore(1 if config.serial else os.cpu_count())
 
-  node_total = len(all_tasks)
+  node_total = len(hancho_tasks)
 
+  all_ok = True
   async def wait(tasks):
     results = await asyncio.gather(*tasks)
-    global any_failed
     for r in results:
-      if r: any_failed = True
-  hancho_loop.run_until_complete(wait(all_tasks))
-  if node_built and not config.verbose: print()
-  #print(f"any_failed = {any_failed}")
-  #print(f"node_total {node_total}")
-  #print(f"node_count {node_visit}")
-  #print(f"node_built {node_built}")
+      if r: all_ok = False
 
-atexit.register(hancho_atexit)
+  hancho_loop.run_until_complete(wait(hancho_tasks))
+  if node_built and not config.verbose: print()
+  reset()
+  return all_ok
 
 ################################################################################
 
