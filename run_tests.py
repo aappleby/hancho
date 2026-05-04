91dc0b57391e61dc4b35418a3247ce61d2e365c5
Austin Appleby <778399+aappleby@users.noreply.github.com>
2026-05-03 22:46:21 -0700

    checkpoint

--- run_tests.py
+++ run_tests.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
 import sys
 import unittest
-import doctest
-import hancho
-import importlib
 
 TEST_MODULES = [
     "tests.test_dict",
@@ -11,43 +8,18 @@ TEST_MODULES = [
     "tests.test_split",
 ]
 
-#def run_file_doctests(filename):
-#    suite = doctest.DocFileSuite(filename, optionflags=doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS)
-#    runner = unittest.TextTestRunner(verbosity=2)
-#    result = runner.run(suite)
-#    if not result.wasSuccessful():
-#        print(f"Doctests in {filename} failed!")
-#        print(f"Result: {result}")
-#        sys.exit(1)
-
-#def run_module_doctests(module):
-#    suite = unittest.TestSuite()
-#    suite.addTests(doctest.DocTestSuite(module, optionflags=doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS))
-#    runner = unittest.TextTestRunner(verbosity=2)
-#    result = runner.run(suite)
-#    if not result.wasSuccessful():
-#        print(f"{module.__name__} doctests failed!")
-#        print(f"Result: {result}")
-#        sys.exit(1)
-
 def run_test_suite(mod_name):
     loader = unittest.TestLoader()
     runner = unittest.TextTestRunner(verbosity=2)
-    suite = loader.loadTestsFromName(mod_name)
-
+    suite  = loader.loadTestsFromName(mod_name)
     result = runner.run(suite)
+
     if not result.wasSuccessful():
         print(f"Test suite {mod_name} failed!")
         print(f"Result: {result}")
         sys.exit(1)
 
 if __name__ == "__main__":
-    #print("Running hancho module doctests")
-    #run_module_doctests(hancho)
-
-    #print("Running external doctests")
-    #run_file_doctests("tests/doctest_all.txt")
-
     for mod in TEST_MODULES:
         print(f"Running {mod}")
         run_test_suite(mod)
